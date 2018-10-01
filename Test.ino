24d17a55ecbf1721db8d3f5278e7c8955a4990a2
Jin <js9202@nyu.edu>
2018-09-30 23:27:14 -0400

    Create Test.ino
    For Ardino

---
+++ Test.ino
@@ -0,0 +1,40 @@
+String incomingByte = "";
+
+void setup() {
+ // put your setup code here, to run once:
+ //Serial1.begin(9600);
+ Serial.begin(9600);
+ pinMode(3,OUTPUT);
+ pinMode(4,OUTPUT);
+ pinMode(8,OUTPUT);
+ digitalWrite(3,HIGH);
+ digitalWrite(4, HIGH);
+}
+
+/* Ports where N is the number
+* N->DN
+*/
+
+//
+//8 is ultrasonic
+
+void loop() {
+ // put your main code here, to run repeatedly:
+ if (Serial.available() > 0) {
+   // read the incoming byte:
+   incomingByte = Serial.readString();
+   if(incomingByte=="w"){
+     digitalWrite(3, HIGH);
+   }
+   if(incomingByte=="a"){
+     digitalWrite(4, LOW);
+   }
+   if(incomingByte=="s"){
+     digitalWrite(3, LOW);
+   }
+   if(incomingByte=="d"){
+     digitalWrite(4, HIGH);
+   }
+   Serial.println(incomingByte);
+ }
+}
